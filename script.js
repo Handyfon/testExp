e9c568d8c5d4e5dd2a9728726a33ea74b719915a
Handyfon <Handyfon@hotmail.com>
2023-03-23 08:57:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -111,8 +111,8 @@ window.addEventListener("message", function (event) {
 
   for (var z = 0; z < event.data.traits.length; z++) {
     var textArea = checkContent("textarea", "name", "features", false)[0];
-    textArea.innerHTML += "Feature: " + event.data.traits[z].trait + "\r\n";
-    textArea.innerHTML += "Description: " + event.data.traits[z].description+ "\r\n\n";
+    textArea.innerHTML += "<b>Feature: " + event.data.traits[z].trait + "</b>\r\n";
+    textArea.innerHTML += event.data.traits[z].description+ "\r\n\n";
   }
 
   if (
